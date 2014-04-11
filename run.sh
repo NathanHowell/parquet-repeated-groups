af40a0f3b1f4c2d039cab72f554b6ad37d548167
Nathan Howell <nhowell@godaddy.com>
2014-04-11 10:07:28 -0700

    Invoke hive once per select *

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm -rf /tmp/parquet/
+HIVE_BUNDLE=parquet-hive-bundle-1.4.2-SNAPSHOT.jar
 mvn exec:java -Dexec.mainClass=com.godaddy.parquet.Main
 diff <(parquet-schema /tmp/parquet/avro/avro.gz.parquet) <(parquet-schema /tmp/parquet/proto/proto.gz.parquet)
 hive -hiveconf hive.root.logger=INFO,console -f /dev/stdin << EOF
-ADD JAR parquet-hive-bundle-1.4.2-SNAPSHOT.jar;
-ADD JAR parquet-hive-binding-bundle-1.4.2-SNAPSHOT.jar;
+ADD JAR ${HIVE_BUNDLE};
 
 DROP TABLE avro;
 DROP TABLE proto;
@@ -24,7 +24,14 @@ INPUTFORMAT 'parquet.hive.DeprecatedParquetInputFormat'
 OUTPUTFORMAT 'parquet.hive.DeprecatedParquetOutputFormat'
 LOCATION '/tmp/parquet/proto/'
 TBLPROPERTIES ('parquet.compression'='gzip', 'parquet.enable.dictionary'='true');
+EOF
 
+hive -hiveconf hive.root.logger=INFO,console -f /dev/stdin << EOF
+ADD JAR ${HIVE_BUNDLE};
 SELECT * FROM avro;
+EOF
+
+hive -hiveconf hive.root.logger=INFO,console -f /dev/stdin << EOF
+ADD JAR ${HIVE_BUNDLE};
 SELECT * FROM proto;
 EOF
